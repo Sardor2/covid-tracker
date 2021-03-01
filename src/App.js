a583ebeec0900dd1ca16bd6ba0dbd19438ea3fb9
unknown <abdurakhimov.sardor@gmail.com>
2021-03-01 23:04:10 +0500

    removed unused components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header.component';
 import HomePage from "./pages/home-page/home-page.component";
-import Spinner from './components/spinner/spinner'
 
 class App extends React.Component  {
   
