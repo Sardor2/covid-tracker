39a890e2bb9c65fe68fa4c25e636d23124e2db30
unknown <abdurakhimov.sardor@gmail.com>
2021-03-02 10:47:24 +0500

    Updates

---
+++ precache-manifest.77ec97565a651618e66f4f8ea63dce18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606a231a15ed22ece3f099bc557be54a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "1e6109f67252ae1374fa",
+    "url": "/covid-tracker/static/css/main.a673bcc1.chunk.css"
+  },
+  {
+    "revision": "48296f1514f959233a4c",
+    "url": "/covid-tracker/static/js/2.bfd06a61.chunk.js"
+  },
+  {
+    "revision": "1f856904e7e7ca93e4849cc435183cdc",
+    "url": "/covid-tracker/static/js/2.bfd06a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6109f67252ae1374fa",
+    "url": "/covid-tracker/static/js/main.c4f4a421.chunk.js"
+  },
+  {
+    "revision": "d2802b1a91cdb5a4a126",
+    "url": "/covid-tracker/static/js/runtime-main.4b074e74.js"
+  },
+  {
+    "revision": "2a6693aa586841899d28762393eca7cb",
+    "url": "/covid-tracker/static/media/patient.2a6693aa.svg"
+  },
+  {
+    "revision": "cd412084bb0493bc3ab8cf4cddd7cc71",
+    "url": "/covid-tracker/static/media/sick.cd412084.svg"
+  },
+  {
+    "revision": "ac8afe634f0f04860c1930ed3978da21",
+    "url": "/covid-tracker/static/media/skull.ac8afe63.svg"
+  }
+]);
